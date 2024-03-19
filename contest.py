19f79f29d962f32321d1dba93064282c6c5efaca
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2024-03-19 15:51:19 -0400

    slight rewrite and add a loop method

--- contest.py
+++ contest.py
@@ -1,23 +1,59 @@
+from datetime import datetime
 import pyperclip
 import requests
 import sys
+import time
 
-if len(sys.argv) >= 3:
-    year = int(sys.argv[2])
-    contest = sys.argv[1]
-else:
-    year = 2024
-    contest = "CCC"
+def check(url):
+    r = requests.get(url)
 
-url = 'https://www.cemc.uwaterloo.ca/contests/past_contests/{}/{}{}Results.pdf'.format(year, year, contest)
+    if r.status_code == 404:
+        return 1
+    elif r.status_code == 200:
+        return 0
+    else:
+        return -1
 
-r = requests.get(url)
+def interact(contest, year):
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    url = 'https://www.cemc.uwaterloo.ca/contests/past_contests/{}/{}{}Results.pdf'.format(year, year, contest)
 
-if r.status_code == 404:
-    print('{} {} results are not out'.format(contest, year))
-elif r.status_code == 200:
-    print('\033[31mHOLY FUCKING SHIT I THINK {} {} RESULTS ARE OUT!!!\033[0m'.format(contest, year))
-    pyperclip.copy(url)
-    print('URL copied to clipboard')
-else:
-    print('Code {} (weird, shouldn\'t happen)'.format(r.status_code))
+    if check(url) == 1:
+        print('{}: {} {} results are not out'.format(now, contest, year))
+        return 1
+    else:
+        print('{}: \033[31mHOLY FUCKING SHIT I THINK {} {} RESULTS ARE OUT!!!\033[0m'.format(now, contest, year))
+        pyperclip.copy(url)
+        print('URL copied to clipboard')
+        return 0
+
+def loop(contests, delay):
+    while True:
+        results = []
+
+        for contest, year in contests:
+            result = interact(contest, year)
+            results.append(result)
+
+        if sum(results) != len(contests):
+            break
+
+        time.sleep(delay)
+        print()
+
+    print('[Loop ended]')
+
+    while True:
+        print(chr(7), end='')
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 3:
+        year = int(sys.argv[2])
+        contest = sys.argv[1]
+    else:
+        year = 2024
+        contest = "CCC"
+
+    interact(contest, year)
